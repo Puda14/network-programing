22c1a4bd4bf179bdbf279d2d54cb0eabab163d53
Puda14 <pdathp14@gmail.com>
2024-10-21 19:37:22 +0700

    game_v3

--- week7/assignments/client.c
+++ week7/assignments/client.c
@@ -8,77 +8,76 @@
 #define BUFFER_SIZE 1024
 
 int main() {
-    int sock = 0;
-    struct sockaddr_in serv_addr;
-    char buffer[BUFFER_SIZE] = {0};
-    int row, col;
-    int input_status; // Variable to check input status
+  int sock = 0;
+  struct sockaddr_in serv_addr;
+  char buffer[BUFFER_SIZE] = {0};
+  int row, col;
+  int input_status; // Variable to check input status
 
-    // 3.1 Set up connection
-    // Create TCP socket
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("Socket creation error\n");
-        return -1;
-    }
+  // 3.1 Set up connection
+  // Create TCP socket
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    printf("\033[31mSocket creation error\033[0m\n");
+  }
 
-    // Configure server address
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+  // Configure server address
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(PORT);
 
-    // Convert server address to binary form
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        printf("Invalid address/ Address not supported\n");
-        return -1;
-    }
+  // Convert server address to binary form
+  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+    printf("\033[31mInvalid address/ Address not supported\033[0m\n");
+    return -1;
+  }
 
-    // Connect to server
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("Connection Failed\n");
-        return -1;
-    }
+  // Connect to server
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    printf("\033[31mConnection Failed\033[0m\n");
+    return -1;
+  }
 
-    while (1) {
-        // 3.2 Receive turn notification
-        recv(sock, buffer, BUFFER_SIZE, 0);
+  while (1) {
+    // 3.2 Receive turn notification
+    recv(sock, buffer, BUFFER_SIZE, 0);
 
-        if (strncmp(buffer, "TURN", 4) == 0) {
-            do {
-                printf("Enter your move (row and column): ");
-                input_status = scanf("%d %d", &row, &col);
+    if (strncmp(buffer, "TURN", 4) == 0) {
+      do {
+        printf("\033[34mEnter your move (row and column): \033[0m");
+        input_status = scanf("%d %d", &row, &col);
 
-                if (input_status != 2) {
-                    printf("Invalid input, please enter two integers!\n");
+        if (input_status != 2) {
+          printf("\033[31mInvalid input, please enter two integers!\033[0m\n");
 
-                    while (getchar() != '\n');
-                } else if (row < 0 || row > 2 || col < 0 || col > 2) {
-                    printf("Please enter values for row and column between 0 and 2.\n");
-                    input_status = 0;
-                }
-
-            } while (input_status != 2);
-
-            // Create MOVE message to send to server
-            snprintf(buffer, BUFFER_SIZE, "%d %d", row, col);
-            send(sock, buffer, strlen(buffer), 0);
-
-        } else if (strncmp(buffer, "INVALID", 7) == 0) {
-            // 3.2 Receive invalid move notification
-            printf("Invalid move, please try again!\n");
-        } else if (strncmp(buffer, "WIN", 3) == 0) {
-            // 3.3 Receive win notification
-            printf("Player %c has won!\n", buffer[4]);
-            break;
-        } else if (strncmp(buffer, "DRAW", 4) == 0) {
-            // 3.3 Receive draw notification
-            printf("The game is a draw!\n");
-            break;
-        } else {
-            // 3.3 Receive game board update
-            printf("Updated game board:\n%s", buffer);
+          while (getchar() != '\n');
+        } else if (row < 0 || row > 2 || col < 0 || col > 2) {
+          printf("\033[33mPlease enter values for row and column between 0 and 2.\033[0m\n");
+          input_status = 0;
         }
-    }
 
-    // Close connection
-    close(sock);
-    return 0;
+      } while (input_status != 2);
+
+      // Create MOVE message to send to server
+      snprintf(buffer, BUFFER_SIZE, "%d %d", row, col);
+      send(sock, buffer, strlen(buffer), 0);
+
+    } else if (strncmp(buffer, "INVALID", 7) == 0) {
+      // 3.2 Receive invalid move notification
+      printf("\033[31mInvalid move, please try again!\033[0m\n");
+    } else if (strncmp(buffer, "WIN", 3) == 0) {
+      // 3.3 Receive win notification
+      printf("\033[1m\033[31m Player %c has won!\033[0m\n", buffer[4]);
+      break;
+    } else if (strncmp(buffer, "DRAW", 4) == 0) {
+      // 3.3 Receive draw notification
+      printf("\033[33m The game is a draw!\033[0m\n");
+      break;
+    } else {
+      // 3.3 Receive game board update
+      printf("\033[32mUpdated game board:\033[0m\n%s", buffer);
+    }
+  }
+
+  // Close connection
+  close(sock);
+  return 0;
 }
--- week7/assignments/server.c
+++ week7/assignments/server.c
@@ -11,178 +11,180 @@ int game_board[3][3]; // Game board
 
 // Initialize the game board
 void init_game_board() {
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-            game_board[i][j] = 0;
+  for (int i = 0; i < 3; i++)
+    for (int j = 0; j < 3; j++)
+      game_board[i][j] = 0;
 }
 
 // Print the game board on the server
 void print_game_board() {
-    printf("\nGame Board:\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (game_board[i][j] == 1)
-                printf("\033[34m X \033[0m");
-            else if (game_board[i][j] == 2)
-                printf("\033[31m O \033[0m");
-            else
-                printf(" . ");
-        }
-        printf("\n");
+  printf("\nGame Board:\n");
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (game_board[i][j] == 1)
+        printf("\033[34m X \033[0m");
+      else if (game_board[i][j] == 2)
+        printf("\033[31m O \033[0m");
+      else
+        printf(" . ");
     }
+    printf("\n");
+  }
 }
 
 // Send TURN notification to the current player
 void notify_turn(int client_sock) {
-    send(client_sock, "TURN", 5, 0);
+  send(client_sock, "TURN", 5, 0);
 }
 
 void send_game_board(int client1_sock, int client2_sock) {
-    char buffer[BUFFER_SIZE] = {0};
-    int offset = 0;
+  char buffer[BUFFER_SIZE] = {0};
+  int offset = 0;
 
-    // Convert the game board to a string
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (game_board[i][j] == 1)
-                offset += snprintf(buffer + offset, BUFFER_SIZE - offset, "\033[34m X \033[0m"); // X in blue
-            else if (game_board[i][j] == 2)
-                offset += snprintf(buffer + offset, BUFFER_SIZE - offset, "\033[31m O \033[0m"); // O in red
-            else
-                offset += snprintf(buffer + offset, BUFFER_SIZE - offset, " . ");
-        }
-        offset += snprintf(buffer + offset, BUFFER_SIZE - offset, "\n");
+  // Convert the game board to a string
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (game_board[i][j] == 1)
+        offset += snprintf(buffer + offset, BUFFER_SIZE - offset, "\033[34m X \033[0m");
+      else if (game_board[i][j] == 2)
+        offset += snprintf(buffer + offset, BUFFER_SIZE - offset, "\033[31m O \033[0m");
+      else
+        offset += snprintf(buffer + offset, BUFFER_SIZE - offset, " . ");
     }
+    offset += snprintf(buffer + offset, BUFFER_SIZE - offset, "\n");
+  }
 
-    send(client1_sock, buffer, BUFFER_SIZE, 0);
-    send(client2_sock, buffer, BUFFER_SIZE, 0);
+  send(client1_sock, buffer, BUFFER_SIZE, 0);
+  send(client2_sock, buffer, BUFFER_SIZE, 0);
 }
 
 int check_winner() {
-    for (int i = 0; i < 3; i++) {
-        if (game_board[i][0] == game_board[i][1] && game_board[i][1] == game_board[i][2] && game_board[i][0] != 0)
-            return game_board[i][0];
-        if (game_board[0][i] == game_board[1][i] && game_board[1][i] == game_board[2][i] && game_board[0][i] != 0)
-            return game_board[0][i];
-    }
-    if (game_board[0][0] == game_board[1][1] && game_board[1][1] == game_board[2][2] && game_board[0][0] != 0)
-        return game_board[0][0];
-    if (game_board[0][2] == game_board[1][1] && game_board[1][1] == game_board[2][0] && game_board[0][2] != 0)
-        return game_board[0][2];
-    return 0;
+  for (int i = 0; i < 3; i++) {
+    if (game_board[i][0] == game_board[i][1] && game_board[i][1] == game_board[i][2] && game_board[i][0] != 0)
+      return game_board[i][0];
+    if (game_board[0][i] == game_board[1][i] && game_board[1][i] == game_board[2][i] && game_board[0][i] != 0)
+      return game_board[0][i];
+  }
+  if (game_board[0][0] == game_board[1][1] && game_board[1][1] == game_board[2][2] && game_board[0][0] != 0)
+    return game_board[0][0];
+  if (game_board[0][2] == game_board[1][1] && game_board[1][1] == game_board[2][0] && game_board[0][2] != 0)
+    return game_board[0][2];
+  return 0;
 }
 
 int main() {
-    int server_fd, client1_fd, client2_fd;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE] = {0};
-    int move_count = 0;
-    int current_player = 1; // Player 1 starts
+  int server_fd, client1_fd, client2_fd;
+  struct sockaddr_in address;
+  int addrlen = sizeof(address);
+  char buffer[BUFFER_SIZE] = {0};
+  int move_count = 0;
+  int current_player = 1; // Player 1 starts
 
-    // 2.1 Set up connection
-    // Create socket
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket failed");
-        exit(EXIT_FAILURE);
+  // 2.1 Set up connection
+  // Create socket
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    perror("Socket failed");
+    exit(EXIT_FAILURE);
+  }
+
+  // Configure server address
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(PORT);
+
+  // Bind socket to address
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    perror("Bind failed");
+    exit(EXIT_FAILURE);
+  }
+
+  // Listen for connections
+  if (listen(server_fd, 2) < 0) {
+    perror("Listen failed");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("\033[33mWaiting for two players to connect...\033[0m\n");
+
+  // Accept connection from player 1 and player 2
+  if ((client1_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+    perror("Accept client 1 failed");
+    exit(EXIT_FAILURE);
+  }
+  printf("\033[32mPlayer 1 has connected.\033[0m\n");
+
+  if ((client2_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+    perror("Accept client 2 failed");
+    exit(EXIT_FAILURE);
+  }
+  printf("\033[32mPlayer 2 has connected.\033[0m\n");
+
+  // Initialize the game board
+  init_game_board();
+
+  // Game loop
+  while (1) {
+    int row, col, winner;
+
+    // 2.2 Alternate turns
+    // Notify the current player of their turn
+    if (current_player == 1) {
+      notify_turn(client1_fd);
+      recv(client1_fd, buffer, BUFFER_SIZE, 0);
+    } else {
+      notify_turn(client2_fd);
+      recv(client2_fd, buffer, BUFFER_SIZE, 0);
     }
 
-    // Configure server address
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    sscanf(buffer, "%d %d", &row, &col);
 
-    // Bind socket to address
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Bind failed");
-        exit(EXIT_FAILURE);
+    // 2.3 Validate move
+    // Check if the move is valid
+    if (game_board[row][col] == 0) {
+      game_board[row][col] = current_player;
+      move_count++;
+    } else {
+      // Send invalid move notification
+      if (current_player == 1) {
+        send(client1_fd, "INVALID", 8, 0);
+      } else {
+        send(client2_fd, "INVALID", 8, 0);
+      }
+      continue;
     }
 
-    // Listen for connections
-    if (listen(server_fd, 2) < 0) {
-        perror("Listen failed");
-        exit(EXIT_FAILURE);
+    print_game_board();
+
+    // 2.3 Update game board
+    // Send the updated game board to both clients
+    send_game_board(client1_fd, client2_fd);
+
+    // 2.4 Check for win or draw
+    winner = check_winner();
+    if (winner > 0) {
+      // Send win notification
+      snprintf(buffer, BUFFER_SIZE, "WIN %d", winner);
+      send(client1_fd, buffer, BUFFER_SIZE, 0);
+      send(client2_fd, buffer, BUFFER_SIZE, 0);
+      printf("\033[32mPlayer %d wins!\033[0m\n", winner);
+      break;
     }
 
-    printf("Waiting for two players to connect...\n");
-
-    // Accept connection from player 1 and player 2
-    if ((client1_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
-        perror("Accept client 1 failed");
-        exit(EXIT_FAILURE);
-    }
-    printf("Player 1 has connected.\n");
-
-    if ((client2_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
-        perror("Accept client 2 failed");
-        exit(EXIT_FAILURE);
-    }
-    printf("Player 2 has connected.\n");
-
-    // Initialize the game board
-    init_game_board();
-
-    // Game loop
-    while (1) {
-        int row, col, winner;
-
-        // 2.2 Alternate turns
-        // Notify the current player of their turn
-        if (current_player == 1) {
-            notify_turn(client1_fd);
-            recv(client1_fd, buffer, BUFFER_SIZE, 0);
-        } else {
-            notify_turn(client2_fd);
-            recv(client2_fd, buffer, BUFFER_SIZE, 0);
-        }
-
-        sscanf(buffer, "%d %d", &row, &col);
-
-        // 2.3 Validate move
-        // Check if the move is valid
-        if (game_board[row][col] == 0) {
-            game_board[row][col] = current_player;
-            move_count++;
-        } else {
-            // Send invalid move notification
-            if (current_player == 1) {
-                send(client1_fd, "INVALID", 8, 0);
-            } else {
-                send(client2_fd, "INVALID", 8, 0);
-            }
-            continue;
-        }
-
-        print_game_board();
-
-        // 2.3 Update game board
-        // Send the updated game board to both clients
-        send_game_board(client1_fd, client2_fd);
-
-        // 2.4 Check for win or draw
-        winner = check_winner();
-        if (winner > 0) {
-            // Send win notification
-            snprintf(buffer, BUFFER_SIZE, "WIN %d", winner);
-            send(client1_fd, buffer, BUFFER_SIZE, 0);
-            send(client2_fd, buffer, BUFFER_SIZE, 0);
-            break;
-        }
-
-        // Check for draw
-        if (move_count == 9) {
-            send(client1_fd, "DRAW", BUFFER_SIZE, 0);
-            send(client2_fd, "DRAW", BUFFER_SIZE, 0);
-            break;
-        }
-
-        // Switch player
-        current_player = (current_player == 1) ? 2 : 1;
+    // Check for draw
+    if (move_count == 9) {
+      send(client1_fd, "DRAW", BUFFER_SIZE, 0);
+      send(client2_fd, "DRAW", BUFFER_SIZE, 0);
+      printf("\033[33mThe game is a draw!\033[0m\n");
+      break;
     }
 
-    // Close connections
-    close(client1_fd);
-    close(client2_fd);
-    close(server_fd);
-    return 0;
+    // Switch player
+    current_player = (current_player == 1) ? 2 : 1;
+  }
+
+  // Close connections
+  close(client1_fd);
+  close(client2_fd);
+  close(server_fd);
+  return 0;
 }
